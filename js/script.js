0c790e31a2be29cb55f8179df1bbe8120ce72c03
Seagull <ssean5739@gmail.com>
2021-07-16 17:33:50 +0300

    Improved accessibility and :focus

--- js/script.js
+++ js/script.js
@@ -32,3 +32,32 @@ $(document).ready(function() {
    });
 
 });
+
+const burger = document.querySelector(".header__humburger-menu");
+const header_links = document.querySelector(".header__links");
+const header_links_close = document.querySelector(".header__links-close");
+
+const closeBurger = function() {
+  header_links.classList.remove("open");
+  header_links_close.classList.remove("open");
+};
+
+const openBurger = function() {
+  header_links.classList.add("open");
+  header_links_close.classList.add("open");
+};
+
+burger.addEventListener("click", function(evt) {
+  evt.stopPropagation();
+  openBurger();
+});
+
+header_links_close.addEventListener("click", function() {
+  closeBurger();
+});
+
+document.addEventListener("click", function(evt) {
+  if (evt.target.className != "header__links" && header_links.classList.contains("open")) {
+    closeBurger();
+  }
+});
